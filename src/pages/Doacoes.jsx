545f6baa7bf02d3e4ada7ebdaff426a2d554af25
caiquefgoncalves <caiquefachinigoncalves@gmail.com>
2026-05-11 21:14:33 -0300

    Add files via upload

---
+++ src/pages/Doacoes.jsx
@@ -0,0 +1,9 @@
+import FazerDoacao from "../components/FazerDoacao/FazerDoacao.jsx";
+
+export default function Doacoes(){
+    return (
+        <>
+            <FazerDoacao/>
+        </>
+    )
+}
